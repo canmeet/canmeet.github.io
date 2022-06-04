b769c55b6a5efc56150ffd677cf068689cee877c
Xiaoming <84471360+WowShiba@users.noreply.github.com>
2022-06-04 19:33:12 +0800

    Grace (#1)
    * add login function
    
    * 更新js
    
    * 更新js
    
    * Update 'Login' page
    
    Co-authored-by: grace1287986 <grace1287986@gmail.com>
    Co-authored-by: Xiaoming <oopsdxm@gmail.com>

---
+++ login/login.js
@@ -0,0 +1,21 @@
+document.getElementById("btn_login_page").addEventListener("click", fnSend);
+
+function fnSend(){
+    let requestBody = { "email": $("#email").val(), "password": $("#password").val() };
+    $.ajax('https://canmeet.herokuapp.com/v1/user/login', {
+        type: 'POST',  // http method
+        dataType: "json",
+        data: JSON.stringify({ email: $("#email").val(), "password": $("#password").val() }),  // data to submit
+        headers: {
+            "Content-Type": "application/json"
+        },
+
+        success: function (data, status, xhr) {
+            document.cookie = `Authorization=Bearer ${data.token}`;
+            $('#myid').html('登入成功 Status Code: ' + xhr.status + ', data: ' + JSON.stringify(data));
+        },
+        error: function (e) {
+            $('#myid').html('登入失敗, Status Code: '+e.status+ ', data: ' + JSON.stringify(e.responseJSON));
+        }
+    });
+}
