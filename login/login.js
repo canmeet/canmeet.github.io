d2af197bc64abee1afa49df62de76e0c7875aa70
grace1287986 <grace1287986@gmail.com>
2022-06-04 08:56:58 +0800

    更新js

--- login/login.js
+++ login/login.js
@@ -1,11 +1,18 @@
-function validate(){
-    var email = document.getElementById("email").value;
-    var password = documet.getElementById("password").value;
-    if (email == "a123@abcbc.com" && password =="123456"){
-        alert("Login successfully");
-    }
-    else{
-        alert("Login failed");
-    }
-    return false
+function fnSend(){
+    let requestBody = { "email": $("#email").val(), "password": $("#password").val() };
+    $.ajax('https://app.swaggerhub.com/apis-docs/CanMeet/CanMeet-API/1.0.0#', {
+        type: 'POST',  // http method
+        dataType: "json",
+        data: JSON.stringify({ email: $("#email").val(), "password": $("#password").val() }),  // data to submit
+        headers: {
+            "Content-Type": "application/json"
+        },
+
+        success: function (data, status, xhr) {
+            $('#myid').html('登入成功 Status Code: ' + xhr.status + ', data: ' + JSON.stringify(data));
+        },
+        error: function (e) {
+            $('#myid').html('登入失敗, Status Code: '+e.status+ ', data: ' + JSON.stringify(e.responseJSON));
+        }
+    });
 }
